f119cfa7f93fbeb0cb76143ac91d779b19afdc47
lxw0109 <lxw0109@gmail.com>
2018-06-06 23:56:01 +0800

    update plot_hist() in with_LSTM.py

--- src/with_LSTM.py
+++ src/with_LSTM.py
@@ -88,7 +88,7 @@ def plot_hist():
     import matplotlib.pyplot as plt
 
     history = None
-    with open("../data/output/hist_obj.pkl", "rb") as f:
+    with open("../data/output/history.pkl", "rb") as f:
         history = pickle.load(f)
     if not history:
         return
@@ -143,6 +143,7 @@ if __name__ == "__main__":
 
     model_train_val(X_train, X_val, y_train, y_val)
     """
+    plot_hist()
     model = load_model("../data/output/models/lstm.model")
     model_predict(model, X_test, X_test_id, X_val, y_val)
     """
