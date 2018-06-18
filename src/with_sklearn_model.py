7447a2c94f1b8867572d42fcdc093b86c34701c1
lxw0109 <lxw0109@gmail.com>
2018-06-18 10:30:29 +0800

    update

--- src/with_sklearn_model.py
+++ src/with_sklearn_model.py
@@ -8,9 +8,10 @@ import numpy as np
 import pandas as pd
 
 from sklearn import linear_model
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
-# from src.preprocessing import gen_train_eval_test_data
+# from src.preprocessing import gen_train_val_test_data
 from preprocessing import gen_train_val_test_data
 
 
@@ -27,13 +28,11 @@ def train_val_predict(X_train, X_val, X_test, X_test_id, y_train, y_val):
     # 2. [NO]NB: 也是著名的机器学习算法, 该方法的任务是还原训练样本数据的分布密度, 其在多分类中有很好的效果
     from sklearn import naive_bayes
     model = naive_bayes.GaussianNB()  # 高斯贝叶斯
-    '''
 
-    # 3. [OK_but_too_slow]KNN:
+    # 3. [OK]KNN:
     from sklearn.neighbors import KNeighborsClassifier
     model = KNeighborsClassifier()  # 非常慢，感觉没法用(跑了半个多小时没反应)
 
-    '''
     # 4. [OK]决策树: 分类与回归树(Classification and Regression Trees, CART)算法常用于特征含有类别信息
     # 的分类或者回归问题，这种方法非常适用于多分类情况
     from sklearn.tree import DecisionTreeClassifier
@@ -52,17 +51,20 @@ def train_val_predict(X_train, X_val, X_test, X_test_id, y_train, y_val):
     model = MLPClassifier(activation="tanh", solver="adam", alpha=0.0001)
     '''
 
+    # 7. RF: 随机森林
+    from sklearn.ensemble import RandomForestClassifier
+    model = RandomForestClassifier(n_jobs=5, random_state=0)
+
     model.fit(X_train, y_train)
-    y_eval_pred = model.predict(X_val)
-    print(classification_report(y_val, y_eval_pred))  # y_true, y_pred
+    y_val_pred = model.predict(X_val)
+    print("classification_report:", classification_report(y_val, y_val_pred))  # y_true, y_pred
+    print("Mean accuracy score:", accuracy_score(y_val, y_val_pred))
+    print("model.score:", model.score(X_val, y_val))
+
     predicted = model.predict(X_test)
     # print(predicted)
     # 把categorical数据转为numeric值，得到分类结果
     predicted = np.argmax(predicted, axis=1)
-    """
-    np.savetxt("../data/output/lstm_submission.csv", np.c_[range(1, len(X_test) + 1), predicted], delimiter=",",
-               header="PhraseId,Sentiment", comments="", fmt="%d")
-    """
     predicted = pd.Series(predicted, name="Sentiment")
     submission = pd.concat([X_test_id, predicted], axis=1)
     submission.to_csv("../data/output/lstm_submission_sk_knn.csv", index=False)
@@ -70,7 +72,7 @@ def train_val_predict(X_train, X_val, X_test, X_test_id, y_train, y_val):
 
 if __name__ == "__main__":
     X_train, X_val, X_test, X_test_id, y_train, y_val = gen_train_val_test_data()
-    print("X_train.shape:{0}\nX_eval.shape:{1}\nX_test.shape:{2}\nX_test_id.shape:{3}\ny_train.shape:{4}\ny_eval.shape:{5}\n".\
+    print("X_train.shape:{0}\nX_val.shape:{1}\nX_test.shape:{2}\nX_test_id.shape:{3}\ny_train.shape:{4}\ny_val.shape:{5}\n".\
         format(X_train.shape, X_val.shape, X_test.shape, X_test_id.shape, y_train.shape, y_val.shape))
     train_val_predict(X_train, X_val, X_test, X_test_id, y_train, y_val)
 
